09d5328fbb60e16ad02e4cab85659df3e592e03c
Bartorz <pochbar@gmail.com>
2023-03-06 23:35:51 +0100

    adding rain to cloud

--- src/iconsSelector/Clouds.tsx
+++ src/iconsSelector/Clouds.tsx
@@ -1,12 +1,50 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { HiCloud } from 'react-icons/hi';
 import { BsFillDropletFill } from 'react-icons/bs';
 import { motion, useAnimation, useTransform } from 'framer-motion';
 
+type RainDropProps = {
+  delayMs: number;
+  leftPosition: number;
+};
+
+const RainDrop = ({ delayMs, leftPosition }: RainDropProps) => {
+  console.log(leftPosition);
+
+  const animation = useAnimation();
+  useEffect(() => {
+    animation.start({
+      top: '110%',
+      left: leftPosition + 'px',
+      transition: { repeat: Infinity, duration: 1, delay: delayMs / 10 },
+    });
+  });
+
+  return (
+    <>
+      <motion.div
+        className='rainDrop'
+        animate={animation}
+        initial={{
+          top: '60%',
+          left: leftPosition + 'px',
+          position: 'absolute',
+          fontSize: 10,
+        }}>
+        <BsFillDropletFill />
+      </motion.div>
+    </>
+  );
+};
+
 export const CloudsType1 = () => {
   const animation = useAnimation();
   useEffect(() => {
-    animation.start({ left: '52%', top: '55%', transition: { duration: 1 } });
+    animation.start({
+      left: '52%',
+      top: '55%',
+      transition: { duration: 1.5 },
+    });
   });
 
   return (
@@ -19,38 +57,30 @@ export const CloudsType1 = () => {
     </motion.div>
   );
 };
-export const Rain = ({ rain }: { rain: number }) => {
-  const rainDropAmount = 2 + Math.round(rain);
-  const animation = useAnimation();
-  useEffect(() => {
-    animation.start({
-      top: '110%',
-      transition: { repeat: Infinity },
-    });
-  });
+export const Rain = ({
+  rain,
+  cloud,
+}: {
+  rain: number;
+  cloud: React.MutableRefObject<null> | React.MutableRefObject<HTMLDivElement>;
+}) => {
+  if (cloud.current == null) return <></>;
+
+  const { offsetLeft, offsetTop, offsetWidth, offsetHeight } = cloud.current;
+  const rainDropAmount = 2 + Math.round(rain) > 8 ? 8 : 2 + Math.round(rain);
+  const rainStep = Math.floor(offsetWidth / rainDropAmount);
+  console.log(offsetLeft, offsetTop, offsetWidth, offsetHeight, rainStep);
 
   return (
     <>
       {Array(rainDropAmount)
-        .fill(1)
-        .map((_, i) => {
-          console.log(i);
-
-          return (
-            <motion.div
-              className='rain'
-              animate={animation}
-              initial={{
-                top: '60%',
-                left: `${50 + i}%`,
-                position: 'absolute',
-                transitionDelay: `${i}00ms`,
-                fontSize: 10,
-              }}>
-              <BsFillDropletFill />
-            </motion.div>
-          );
-        })}
+        .fill(5)
+        .map((_, i) => (
+          <RainDrop
+            delayMs={i * Math.random() * 10}
+            leftPosition={i * rainStep}
+          />
+        ))}
     </>
   );
 };
@@ -58,22 +88,22 @@ export const Rain = ({ rain }: { rain: number }) => {
 export const RainyCloud = () => {
   const animation = useAnimation();
   const [rain, setrain] = useState(false);
-
+  const cloudRef = useRef(null);
   useEffect(() => {
     animation.start({ left: '52%', top: '55%', transition: { duration: 1 } });
     setTimeout(() => {
       setrain(true);
     }, 1000);
   });
-
   return (
     <motion.div
+      ref={cloudRef}
       initial={{ left: '110%', fontSize: 60, top: '55%' }}
       whileHover={{ top: '53%' }}
       animate={animation}
       className='weatherIconContainer cloud'>
       <HiCloud />
-      <Rain rain={10} />
+      <Rain rain={10} cloud={cloudRef} />
     </motion.div>
   );
 };
